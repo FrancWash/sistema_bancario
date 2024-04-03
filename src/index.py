8809364ef4d9df2ab7ad881c3e84c40177eb0757
FrancWash <fvilela216@gmail.com>
2024-04-03 18:52:43 -0300

    Criando variaveis para o projeto do sistema bancario

---
+++ src/index.py
@@ -0,0 +1,15 @@
+#Usando string multilinha
+menu = """
+
+[d] Deposito
+[s] Sacar
+[e] Extrato
+[s] Sair
+
+"""
+#criando variaveis conforme pede o desafio de sistema bancario primeira versão 
+saldo = 0
+limite = 500
+extrato = ""
+numero_saque = 0
+limite_saque = 3
